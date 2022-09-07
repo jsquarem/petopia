596738c047801960382b4e6a2fca66498b4fcb8c
jmartymar <jeffjmart@gmail.com>
2022-09-07 14:52:19 -0500

    pagination complete

---
+++ main_app/migrations/0003_remove_profile_id_alter_profile_user.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('main_app', '0002_profile'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='profile',
+            name='id',
+        ),
+        migrations.AlterField(
+            model_name='profile',
+            name='user',
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
