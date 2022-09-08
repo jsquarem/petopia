e3339925671f62a628c85843416d4bb39e534917
jmartymar <jeffjmart@gmail.com>
2022-09-08 17:47:47 -0500

    for sure

--- petopia/settings.py
+++ petopia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+)2qiv65g0kqsv%l0wvq=pt@704b6x1a3!zgqcnge0$_%1q$ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.localhost']
 
 
 # Application definition
