eaa5c4369c835b434d016ebf5c2c1b29b63341fd
jmartymar <jeffjmart@gmail.com>
2022-09-08 17:18:44 -0500

    more chat thanks eli

--- petopia/asgi.py
+++ petopia/asgi.py
@@ -14,10 +14,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-import main_app.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petopia.settings')
 django.setup()
+import main_app.routing
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
     'websocket':AuthMiddlewareStack(
