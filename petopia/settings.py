b96c75ab0e4e12b9cb95b02f7b8ba432f9c65934
jmartymar <jeffjmart@gmail.com>
2022-09-08 19:24:48 -0500

    fix login csrf

--- petopia/settings.py
+++ petopia/settings.py
@@ -130,6 +130,8 @@ LOGIN_REDIRECT_URL = '/profiles/create/'
 
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
+CSRF_TRUSTED_ORIGINS = ['https://pet-opia.herokuapp.com']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
