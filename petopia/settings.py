c95872b18b616e245a3844b12f33e8ad4847bc4c
jmartymar <jeffjmart@gmail.com>
2022-09-08 22:34:16 -0500

    add sendgrid smtp

--- petopia/settings.py
+++ petopia/settings.py
@@ -138,11 +138,11 @@ CSRF_TRUSTED_ORIGINS = ['https://pet-opia.herokuapp.com']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = '1025'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 # EMAIL_USE_SSL = False
 
 
