e14dc200e43016a5a25d623f44738bc3e5c27fca
jmartymar <jeffjmart@gmail.com>
2022-09-09 00:29:43 -0500

    uncomment email env vars

--- petopia/settings.py
+++ petopia/settings.py
@@ -138,11 +138,11 @@ CSRF_TRUSTED_ORIGINS = ['https://pet-opia.herokuapp.com']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
-# EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 # EMAIL_USE_SSL = False
 
 
